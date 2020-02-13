8dccfb9a90d2414fc57690cc10ff29e1f48acd84
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2020-02-14 10:55:20 +1300

    Current

---
+++ 02_LU_generate_token.py
@@ -0,0 +1,38 @@
+# Lucky Unicorn Decomposition Step 2
+# Generate a random token
+
+# To do
+# Set up starting amount
+# Choose 100 tokens (ie: play 100 rounds and...
+#   count # of unicorns and multiply by 5
+#   count # of horse/zebra and multiply by 0.5
+#   count # of donkeys
+# work out total won / lost
+
+
+import random
+
+HOW_MUCH = 100
+tokens = ["Horse", "Zebra", "Donkey", "Unicorn"]
+
+unicorn_count = 0
+zebra_horse_count = 0
+donkey_count = 0
+
+for item in range(0,HOW_MUCH):
+
+    chosen = random.choice(tokens)
+
+    if chosen == "Unicorn":
+        unicorn_count += 1
+    elif chosen == "Donkey":
+        donkey_count += 1
+    else:
+        zebra_horse_count += 1
+
+    print(chosen)
+
+print("***** Win / Loss Calculations *****")
+print("# Unicorns: {}".format(unicorn_count))
+print("# Zebras / Horses: {}".format(zebra_horse_count))
+print("# Donkeys: {}".format(donkey_count))
